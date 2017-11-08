510c64fd01c2f13eae16a1024d2c790e88480db1
鈴木 啓太 <suzukikeita@MacBook-Pro-2.local>
2017-11-08 19:07:35 +0900

    11月8日

--- mysql1.js
+++ mysql1.js
@@ -9,7 +9,7 @@ var mysql_options = {
    user: db_user,
    password: db_pw,
    database: db_name
-};
+}
 var my_client = mysql.createConnection(mysql_options);
 my_client.connect();
 
@@ -19,6 +19,13 @@ var server      = http.createServer();
 var settings    = require('./settings');
 var msg;
 var querystring = require('querystring')
+
+var sessionids = {}
+
+//var allCookies = document.cookie
+
+//document.cookie = "Id=sessionid"
+
 server.on('request', function(req, res) {
     switch (req.url) {
         case '/register':
@@ -33,21 +40,20 @@ server.on('request', function(req, res) {
                 console.log("read文を実行しました")
             })
 
-        req.on('end', function() {
-            var body          = querystring.parse(data);
-            var name          = body.name
-            var email         = body.email
-            var password      = body.password
-            var sql_statement = 'insert into user (name, email, password) values ("'+ name +'", '+'"'+ email +'", '+'"'+  password +'")'
+            req.on('end', function() {
+                var body          = querystring.parse(data);
+                var name          = body.name
+                var email         = body.email
+                var password      = body.password
+                var sql_statement = 'insert into user (name, email, password) values ("'+ name +'", '+'"'+ email +'", '+'"'+  password +'")'
 
-            my_client.query(sql_statement, function(err, rows){
-                console.log("クエリ文を実行しています")
-                res.setHeader("Location", "http://127.0.0.1:1337/login")
-                res.statusCode = 302
-                res.end();
-                console.log("一連の処理が終わりました")
+                my_client.query(sql_statement, function(err, rows){
+                    console.log("クエリ文を実行しています")
+                    res.setHeader("Location", "http://127.0.0.1:1337/login")
+                    res.statusCode = 302
+                    res.end();
+                })
             })
-         })
     msg = "アカウント登録完了画面"
     break
 
@@ -63,36 +69,40 @@ server.on('request', function(req, res) {
                             console.log("read文を実行しました")
         })
         req.on('end', function() {
+            console.log(data)
             var body          = querystring.parse(data);
             var email         = body.email
             var password      = body.password
             var sql_statement = 'SELECT * FROM user WHERE email = "' + email + '" AND password = "' + password + '"'
             my_client.query(sql_statement, function(err, rows){
                 if (rows != 0) {
-                    var vol    = 8
-                    var words  = "abcdefghijklmnopqrstuvwxyz0123456789!#$%&*+-"
-                    var length = words.length
-                    var id     = ""
+                    var vol       = 8
+                    var words     = "abcdefghijklmnopqrstuvwxyz0123456789!#$%&*+-"
+                    var length    = words.length
+                    var sessionid = ""
                     for(var i=0; i<vol; i++){
-                        id += words[Math.floor(Math.random()*length)]
+                        sessionid += words[Math.floor(Math.random()*length)]
                     }
-
-                    var sessionid = {}
-                    sessionid[id] = "true"
+                    sessionids[sessionid] = "true"
+                    //console.log(sessionids)
                     console.log(sessionid)
-                    console.log(id)
                     res.setHeader("Content-Type", "text/plain")
-                    res.setHeader('Set-Cookie',Object.keys(sessionid))
-                    console.log(Object.keys(sessionid))
-                    //console.log(req.headers.cookie)
+                    res.setHeader('Set-Cookie',['sessionid='+ sessionid]+'path=/')
+                    //res.setHeader('Set-Cookie',['aaaa='+ Object.keys(sessionids)])
+                    //console.log(Object.keys(sessionid))
+
+                    var cookie = req.headers.cookie
+                    console.log(cookie)
+
                     res.setHeader("Location", "http://127.0.0.1:1337/time_line")
                     res.statusCode = 302
                     res.end()
-                } else{
+                }
+                else {
                     res.setHeader("Location", "http://127.0.0.1:1337/register")
                     res.statusCode = 302
                     res.end()
-                  }
+                }
             })
         })
 
@@ -102,21 +112,39 @@ server.on('request', function(req, res) {
     case '/time_line':
         var display = fs.readFile("./views/time_line.html","utf-8", doRead)
         var cookie = req.headers.cookie
-      console.log(req.headers)
+        console.log("タイムラインに行きました")
+        console.log(cookie)
+    break
 
+    case '/tweet_post':
+        var display = fs.readFile("./views/tweet_post.html","utf-8", doRead)
+        console.log("ツイート投稿画面に来ました")
+    break
+
+    case '/tweet_post/complete':
+        var data = ''
+        req.on('readable', function(){
+            data += req.read()||"";
+        })
+        req.on('end', function(){
+            console.log(data)
+          var body  = querystring.parse(data)
+          var tweet = body.tweet
+          var sql_statement = 'insert into tweets (tweet) values ("'+tweet+'")'
+
+
+          my_client.query(sql_statement, function(err, rows){
+              console.log("ツイートをデータベースに保存しました")
+          res.setHeader("Location", "http://127.0.0.1:1337/time_line")
+          res.statusCode = 302
+          res.end();
+          })
+     })
     break
 
     case '/user_list':
         msg = "ユーザー一覧"
     break
-
-    case '/tweet_post':
-        msg = "ツイート投稿ページ"
-        break
-
-    case '/tweet_post/complete':
-        msg = "ツイート投稿完了しました"
-        break
 
     default:
        msg = 'エラー';
